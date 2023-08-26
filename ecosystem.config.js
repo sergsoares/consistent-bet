db08b7181b7566980f43973e44db1d227832d5ce
Sergio Soares <acad.sergio@gmail.com>
2023-08-26 20:04:48 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = [
   {
-    script: 'node dist/apps/consistent-bets/main',
+    script: 'npm start:prod',
     name: 'consistent-bets'
+  }
     //exec_mode: 'cluster',
     //instances: 2
-  }, 
-  {
-    script: 'node dist/apps/payments-worker/main',
-    name: 'payments-worker'
-  }
+  // }, 
+  // {
+  //   script: 'node dist/apps/payments-worker/main',
+  //   name: 'payments-worker'
+  // }
 ]
